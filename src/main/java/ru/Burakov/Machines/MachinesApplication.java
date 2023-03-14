5a575138b09a34711d2b54c108d6eec16ad8cf89
Nikita <nikitaburakov08@gmail.com>
2023-03-14 14:51:36 +0500

    Initial commit

---
+++ src/main/java/ru/Burakov/Machines/MachinesApplication.java
@@ -0,0 +1,13 @@
+package ru.Burakov.Machines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/Burakov/Machines/MachinesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Burakov.Machines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
