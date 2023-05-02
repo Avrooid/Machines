454012a4fada2ddf92917d7f893d81cae19d0e1b
Nikita <nikitaburakov08@gmail.com>
2023-05-02 21:27:51 +0500

    Add Security

--- src/main/java/ru/Burakov/Machines/security/SecurityConfig.java
+++ src/main/java/ru/Burakov/Machines/security/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http.csrf().disable().cors().disable()
+                .authorizeHttpRequests()
                 .requestMatchers("/admin/api").hasRole("ADMIN")
                 .requestMatchers("/registration", "/db/carBrand").permitAll()
                 .anyRequest().authenticated()
